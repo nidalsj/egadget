0c4a3b87a659283ae7112044ccebd83e7d7ecaec
Nidal SJ <connectnidalsj@gmail.com>
2023-09-12 12:31:58 +0530

    firstcommit

---
+++ customer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns =[
+    path("cust",CustomerHomeView.as_view(),name="custhome"),
+    path("pdetail/<int:id>",ProductDetailView.as_view(),name="pdet")
+
+
+]
+
